ba10844e6a64b46fe4f0bc7f97d924e649111f45
Julioteixeirafh <juliocezarteifilho@gmail.com>
2025-05-23 21:23:28 -0300

    #1 - feat(): jogo da forca

---
+++ venv/Scripts/aula2_forca.py
@@ -0,0 +1,24 @@
+palavra = "carrossel"
+descobertas = ["*" for _ in palavra]
+tentativas = 11
+
+def forca():
+    exibiir=print("Você iniciou o jogo da FORCA! Você tem:", (tentativas), "tentativas")
+    while "*" in descobertas and tentativas > 0:
+        letra = input("Digite uma letra: ")
+
+        if letra in palavra:
+            for i in range(len(palavra)):
+                if palavra[i] == letra:
+                    descobertas[i] = letra
+            print(" ".join(descobertas))
+        else:
+            tentativas -= 1
+            print(" ".join(descobertas))
+            print("Tentativas restantes:", tentativas)
+
+    if "*" not in descobertas:
+        print("Você acertou a palavra!")
+    else:
+        print("\n\tVocê perdeu! A palavra era:\n  \n\tDragão\n ")
+forca()
